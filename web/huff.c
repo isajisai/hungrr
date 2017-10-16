ab5b57031480b048674657b5864a780382106141
isajisai <silaspsadia@gmail.com>
2017-10-15 21:50:26 -0700

    generalized

---
+++ web/huff.c
@@ -0,0 +1,24 @@
+
+/*
+	Project: Huffman compressor
+	File: huff.c
+	Description: main module
+	Written by pongozolin, isajisai (2017)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ASCII_SIZE		256
+
+typedef struct Node {
+	Node *left;
+	Node *right;
+	int weight;
+	char item;
+}
+
+void scan_file() {
+	Node *dictionary = (Node *)calloc(ASCII_SIZE, size_of(Node));
+}
